16e3723b8d54b3cc381ed5ed1b32118feeb193ac
youngdaragon <93703248+youngdaragon@users.noreply.github.com>
2022-11-22 13:12:32 +0900

    Delete E_or_I directory

--- E_or_I/ifnen.py
+++
@@ -1,6 +0,0 @@
-import torch
-import cv2
-model=torch.load('/home/kimyongtae/yolov5/ckpt_epoch_299.pth')
-img=cv2.imread('/home/kimyongtae/yolov5/test/images3.jpeg')
-
-print(model)
--- E_or_I/testing.py
+++
@@ -1,3 +0,0 @@
-import persondetect as ps
-
-ps.run(weights='yolov5s.pt',source='data/nmask/images/train', save_txt=True, project='result', name='mbti')
